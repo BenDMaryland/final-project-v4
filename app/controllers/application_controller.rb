96e50231a6db15fe9fa108af006fb2b434302374
My Name <ben.d.maryland@gmail.com>
2021-12-01 11:17:32 -0500

    react

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
 
-    include ActionController::Cookies
+
+    
+  def hello_world
+    session[:count] = (session[:count] || 0) + 1
+    render json: { count: session[:count] }
+  end
+
+
+  
 end
