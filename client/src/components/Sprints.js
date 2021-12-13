4f3d5a63130a61b3f926134169ef207af948a60a
My Name <ben.d.maryland@gmail.com>
2021-12-12 19:21:40 -0500

    testing for heroku

--- client/src/components/Sprints.js
+++ client/src/components/Sprints.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
 
 function Sprints({ fetchedSprints }) {
-    console.log(fetchedSprints)
+
+
+
+
     if(!fetchedSprints) return null
+
+
     return (
 
         fetchedSprints.map(sprint => {
             return (
-                <div>
+                <div key={sprint.id}>
                     <h1>{sprint.sprint_title}</h1>
                 </div>
             )
