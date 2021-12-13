3755010cb580fe7c2946548587472de109d862cb
My Name <ben.d.maryland@gmail.com>
2021-12-12 20:34:55 -0500

    test

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import { Routes, Route } from "react-router-dom"
 import { BrowserRouter } from "react-router-dom";
 import {CurrentUserProvider} from "./custom/CurrentUser"
@@ -19,4 +19,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
