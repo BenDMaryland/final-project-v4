defd4b44d3aae1e95f1f948ac6158f402bcc12d3
My Name <ben.d.maryland@gmail.com>
2022-02-14 22:23:53 -0500

    testing

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import styled from 'styled-components';
+
 import { Routes, Route } from "react-router-dom"
 import { BrowserRouter } from "react-router-dom";
 import {CurrentUserProvider} from "./custom/CurrentUser"
